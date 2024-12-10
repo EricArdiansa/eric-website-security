f9fc23e7cf2b5901d69f7e38ce19f4f54a5b0ebb
EricArdiansa <99806966+EricArdiansa@users.noreply.github.com>
2024-12-11 00:58:51 +0700

    add project

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -19,6 +19,30 @@ const projectsData: Project[] = [
     imgSrc: '/static/images/python.png',
     href: 'https://github.com/EricArdiansa/PortScanner',
   },
+  {
+    title: 'Python Keylogger',
+    description: `Simple keylogger using python to log all your target information. We can compile the .py file to executable binary using pyinstaller`,
+    imgSrc: '/static/images/python.png',
+    href: 'https://github.com/EricArdiansa/Python-Keylogger',
+  },
+  {
+    title: 'Simple Webserver',
+    description: `Develop a simple web server using socket programming in C on Linux. It can only send web pages (like .html) and files (like .jpg) and can only accept GET requests without parameter`,
+    imgSrc: '/static/images/c.png',
+    href: 'https://github.com/EricArdiansa/c-program/blob/main/webserver.c',
+  },
+  {
+    title: 'XSS Labs - IDN Pentest+ Bootcamp',
+    description: `A vulnerable website to showcase all the commonly found varians from Cross Site Scripting Attack (Stored XSS, DOM XSS, Reflected XSS).`,
+    imgSrc: '/static/images/php.jpg',
+    href: 'https://github.com/EricArdiansa/IDN-PentestPlus-Bootcamp/tree/main/Week%201%20-%20XSS%20Labs',
+  },
+  {
+    title: 'SQL Injection Labs - IDN Pentest+ Bootcamp',
+    description: `A vulnerable website to showcase commonly found of SQL Injection type (Login Bypass via subverting logic, Blind SQL, UNION Attack, and Retrieve Hidden Data).`,
+    imgSrc: '/static/images/php.jpg',
+    href: 'https://github.com/EricArdiansa/IDN-PentestPlus-Bootcamp/tree/main/Week%202%20-%20SQL%20Injection%20Labs',
+  },
 ]
 
 export default projectsData
